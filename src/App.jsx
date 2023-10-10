894c5aaaac73d48d1d7ab7f9ac69e4ab6bf5dc7a
Eiji-Kei <kristineangelagallawan9@gmail.com>
2023-10-10 19:29:51 +0800

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,60 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Post from "./components/Post"
+import useFetch from './hooks/useFetch'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [number, setNumber] = useState(1)
+  // const [post, setPost] = useState("")
+  // const [count, setCount] = useState(0)
+
+  // const fetchPost = async (number) => {
+  //   try {
+  //     const response = await fetch('https://jsonplaceholder.typicode.com/posts/' + number)
+  //     const data = await response.json()
+
+  //     setPost(data)
+
+  //   } catch (error){
+  //     console.log(error)
+
+  //   } finally {
+  //     console.log('Done')
+  //   }
+
+    
+  // }
+
+  const {data} = useFetch ('https://jsonplaceholder.typicode.com/posts/', number)
+
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/posts/' + number)
+  //   .then((response) => response.json())
+  //   .then((data) => setPost(data))
+  //   .catch((error) => console.log(error))
+
+  //   fetchPost(number)
+
+  // }, [number] )
+
+  useEffect(() => {
+    console.log('Mounted')
+
+    return() => {
+      console.log('Remove')
+    }
+  }, [])
+  
 
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center'>
-      <Post />
+      { data && <Post title={data.title} body={data.body}/> }
+      {number}
       <div className='flex items-center gap-3 mt-3'>
       {Array.from({length:5}).map((value, index) => {
         return(
-          <div className='p-4 border font-bold hover:cursor-pointer'>
+          <div key={`paguination-${index+1}`}className='p-4 border font-bold hover:cursor-pointer' onClick={() => setNumber(index + 1)}>
             {index + 1}
           </div>
         )
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Post = () => {
+const Post = ({title, body}) => {
   return (
       <div className='p-4 bg-white border border-grey-400/30 w-[250px] text-center'>
-        <h4>Title</h4>
-        <p>Body</p>
+        <h4 className='text-2xl font-bold'>{title}</h4>
+        <p>{body}</p>
       </div>
   )
 }
